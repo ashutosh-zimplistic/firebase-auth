e018e0fa0cd1f593be34655f1ea97c79644e7802
Ashutosh Rath <ashutosh.rath@zimplistic.com>
2025-08-20 11:21:07 +0530

    fix: process env issu

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,26 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { config } from 'dotenv';
 
-config();
-
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY  ,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-  };
-
-  // process to make .env work in react
-  // 1. npm install dotenv
-  // 2. create .env.local file
-  // 3. add the variables to the .env.local file
-  // 4. add the following to the top of the file:
-  // import { config } from 'dotenv';
-  // config();
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
